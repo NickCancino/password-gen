e96e1f251bd2c529468b0e7d372e1d47336ad7b4
Nicolas Cancino <nicocancino0@gmail.com>
2026-04-22 16:43:57 -0400

    Continued code and started JS logic

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-const upperChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V",
-    "W","X","Y","Z"];
-const lowerChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",
-    "w","x","y","z"]
-const specialChars = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$",
-    "%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerChars = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?/";
+const allChars = upperChars + lowerChars + numbers + specialChars;
+
+let userNum;
+let cBox1 = document.getElementById("copyBox1")
+let cBox2 = document.getElementById("copyBox2")
+const errorMsg = document.getElementById("errorMsg");
+
+document.getElementById("generatebutton").onclick = function passLength(){
+    userNum = Number(document.getElementById("usernum").value)
+    if (userNum < 6) {
+        errorMsg.textContent = "Number must be at least 6";
+        return;
+    }else if (userNum>15){
+        errorMsg.textContent = "Number must be less than 16";
+        return;
+    }else if(isNaN(userNum)){
+        errorMsg.textContent = "Please enter a number value";
+        return;
+    }
+    errorMsg.textContent = "";
+
+    cBox1.textContent =  genArr(userNum)
+    cBox2.textContent = genArr(userNum)
+    
+}
+
+function genArr(length) {
+    let password = "";
+  
+    const randomValues = new Uint32Array(length);
+    crypto.getRandomValues(randomValues);
+  
+    for (let i = 0; i < length; i++) {
+      const index = randomValues[i] % allChars.length;
+      password += allChars[index];
+    }
+  
+    return password;
+}
+
+
+
+
+
+console.log(userNum)
